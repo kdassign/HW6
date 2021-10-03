65416e2a367c80e67c4eab1b4843da84204ad3f7
Kelsey Duru <kdassignments@gmail.com>
2021-10-02 20:04:21 -0700

    forgot to push some changes, but inserted by api key, got the 5 day forecast, displayed it onto the page, etc

---
+++ script.js
@@ -0,0 +1,65 @@
+const shortDate = (dt) => {
+    const dateObject = new Date(dt * 1000)
+    const humanDateFormat = dateObject.toLocaleString()
+    const sdate = dateObject.toLocaleString('en-US', { weekday: 'long' }) + ', ' + dateObject.toLocaleString('en-US', { month: 'numeric' }) + '/' + dateObject.toLocaleString('en-US', { day: 'numeric' }) + '/' + dateObject.toLocaleString('en-US', { year: 'numeric' })
+    return sdate
+  }
+  
+  const Display = (bold) => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${bold}&units=imperial&appid=d155f4746e7f77e3912e3f7d86e0bc31`)
+      .then(res => {
+        const weather = res.data
+        // console.log(weather)
+        document.getElementById('cityLocation').innerHTML = `${weather.name}`
+        document.getElementById('conditions').innerHTML = `<img src='http://openweathermap.org/img/wn/${weather.weather[0].icon}.png'>`
+        document.getElementById('temp').innerHTML = `${weather.main.temp}`
+        document.getElementById('hum').innerHTML = `${weather.main.humidity}`
+        document.getElementById('wndSpd').innerHTML = `${weather.wind.speed}`
+        document.getElementById('date').innerHTML = Date()
+        // gets 5 day forecast
+        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&units=imperial&appid=d155f4746e7f77e3912e3f7d86e0bc31`)
+          .then(resp => {
+            const weatherc = resp.data.current
+            const future = resp.data.daily
+            const resultName = res.data.name
+  
+            const history = JSON.parse(localStorage.getItem('history')) || []
+            history.push({ resultName })
+            localStorage.setItem('history', JSON.stringify(history))
+            document.getElementById('searchHistory').innerHTML = ''
+            for (let h = history.length - 1; h > -1; h--) {
+              document.getElementById('searchHistory').innerHTML += `
+          <button type="button" class="list-group-item list-group-item-action" data-val="${history[h].resultName}">${history[h].resultName}</button>`
+            }
+            // console.log(history)
+            // console.log(future)
+            for (let i = 0; i < 5; i++) {
+              // inputs 5 day forecast data onto page
+              document.getElementById(`slot-${i + 1}`).innerHTML = `
+          <div class="card">
+          <div class="card-body">
+          <h5 class="card-title">${shortDate(future[i].dt)}</h5>
+          <img src='http://openweathermap.org/img/wn/${weatherc.weather[0].icon}.png'>
+          <h6 class="card-text">Max: ${future[i].temp.max} &degF</h6>
+          <h6 class="card-text">Min: ${future[i].temp.min} &degF</h6>
+          <h6 class="card-text">Humidity: ${future[i].humidity}%</h6>
+          </div>
+          </div>
+          `
+              console.log(weatherc)
+            }
+  
+            // identifies UV index and changes color based on its value
+            document.getElementById('uvInd').innerHTML = `${weatherc.uvi}`
+            if (weatherc.uvi >= 0 && weatherc.uvi < 3) {
+              document.getElementById('uvInd').classList.add('bg-success')
+            } else if (weatherc.uvi >= 3 && weatherc.uvi < 6) {
+              document.getElementById('uvInd').classList.add('bg-warning')
+            } else if (weatherc.uvi >= 6 && weatherc.uvi < 9) {
+              document.getElementById('uvInd').classList.add('bg-danger')
+            } else {
+              document.getElementById('uvInd').classList.add('bg-dark')
+            }
+          })
+      })
+  }
